687dd6da842d079e902ef492b81c87479d635976
wolwesking <83821890+wolwesking@users.noreply.github.com>
2024-01-23 20:11:26 +0500

    finalTouchesOnGetFunction

--- routes/admin.js
+++ routes/admin.js
@@ -1,18 +1,16 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var {getAllRecords} = require('../service/getLeao_service')
+var { getAllRecords } = require("../service/getLeao_service");
 /* GET home page. */
 
-router.get('/', async function(req, res, next) {
+router.get("/", async function (req, res, next) {
   try {
     // Call the function to get all records
     const allRecords = await getAllRecords();
 
-    // Render the 'admin' view with the retrieved data
-    res.render('admin', { data: allRecords });
+    res.render("admin", { data: allRecords });
   } catch (error) {
     console.error("Error in route:", error);
-    // Handle the error as needed, for example, send an error response
     res.status(500).send("Internal Server Error");
   }
 });
