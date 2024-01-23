3ef4423397df747f3d9674ac96ed5ce0a15bd50b
wolwesking <83821890+wolwesking@users.noreply.github.com>
2024-01-23 08:57:44 +0100

    adminPanelTemplate

--- routes/admin.js
+++ routes/admin.js
@@ -3,7 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('admin');
+
+  const data = [
+    { id: 1, NameOfCompany: 'Company A', DateOfIncorporation: new Date() },
+    { id: 2, NameOfCompany: 'Company B', DateOfIncorporation: new Date() },
+  ];
+
+  res.render('admin', {data});
 });
 
 module.exports = router;
